70aa9356f3f120e0ed1815f6b810751cfb326dd5
Evgeniy Polyakov <zbr@ioremap.net>
2019-08-26 15:15:29 +0100

    Moved common code for py_function() wrapper into separate file

---
+++ map_iter.py
@@ -0,0 +1,65 @@
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.util import nest
+
+def py_func(func,
+            args=(),
+            kwargs={},
+            output_types=None,
+            output_shapes=None,
+            name=None):
+    if not isinstance(args, (list, tuple)):
+        raise TypeError('args must be list and not {}. args: {}'.format(type(args), args))
+
+    if not isinstance(kwargs, dict):
+        raise TypeError('kwargs must be dict and not {}. args: {}'.format(type(kwargs), kwargs))
+
+
+    # For dynamic type inference use callable output_types and output_shapes
+    if callable(output_types):
+        # If callable, assume same signature and call with tensors and get the types
+        output_types = output_types(*args, **kwargs)
+    if callable(output_shapes):
+        # If callable, assume same signature and call with tensors and get the shapes
+        output_shapes = output_shapes(*args, **kwargs)
+
+    flat_output_types = nest.flatten(output_types)
+    args = (args, kwargs)
+    flat_args = nest.flatten(args)
+
+
+    def python_function_wrapper(*py_args):
+        py_args, py_kwargs = nest.pack_sequence_as(args, py_args)
+
+        ret = func(*py_args, **py_kwargs)
+        # ToDo: Catch Exceptions and improve msg, because tensorflow ist not able
+        # to preserve the traceback, i.e. the Exceptions does not contain any
+        # information where the Exception was raised.
+        nest.assert_shallow_structure(output_types, ret)
+        return nest.flatten(ret)
+
+    flat_values = tf.py_function(python_function_wrapper, flat_args, flat_output_types, name=name)
+
+    if output_shapes is not None:
+        # I am not sure if this is nessesary
+        output_shapes = nest.map_structure_up_to(output_types, tensor_shape.as_shape, output_shapes)
+
+    flattened_shapes = nest.flatten(output_shapes)
+    for ret_t, shape in zip(flat_values, flattened_shapes):
+        ret_t.set_shape(shape)
+
+    return nest.pack_sequence_as(output_types, flat_values)
+
+def from_indexable(iterator, output_types, output_shapes, num_parallel_calls=None, name=None):
+    ds = tf.data.Dataset.range(len(iterator))
+
+    def index_to_entry(index):
+        return py_func(
+                func=iterator.__getitem__,
+                args=(index,),
+                output_types=output_types,
+                output_shapes=output_shapes,
+                name=name)
+
+    return ds.map(index_to_entry, num_parallel_calls=num_parallel_calls)
+
+
--- segmentation.py
+++ segmentation.py
@@ -11,6 +11,7 @@ import tensorflow_datasets as tfds
 import image as image_draw
 import dataset as ds_impl
 import loss
+import map_iter
 import validate
 import unet
 
@@ -93,70 +94,6 @@ def load_image(datapoint, aug):
     )
 
     return filename, image, mask
-
-from tensorflow.python.framework import tensor_shape
-from tensorflow.python.util import nest
-
-def py_func(func,
-            args=(),
-            kwargs={},
-            output_types=None,
-            output_shapes=None,
-            name=None):
-    if not isinstance(args, (list, tuple)):
-        raise TypeError('args must be list and not {}. args: {}'.format(type(args), args))
-
-    if not isinstance(kwargs, dict):
-        raise TypeError('kwargs must be dict and not {}. args: {}'.format(type(kwargs), kwargs))
-
-
-    # For dynamic type inference use callable output_types and output_shapes
-    if callable(output_types):
-        # If callable, assume same signature and call with tensors and get the types
-        output_types = output_types(*args, **kwargs)
-    if callable(output_shapes):
-        # If callable, assume same signature and call with tensors and get the shapes
-        output_shapes = output_shapes(*args, **kwargs)
-
-    flat_output_types = nest.flatten(output_types)
-    args = (args, kwargs)
-    flat_args = nest.flatten(args)
-
-    
-    def python_function_wrapper(*py_args):
-        py_args, py_kwargs = nest.pack_sequence_as(args, py_args)
-
-        ret = func(*py_args, **py_kwargs)
-        # ToDo: Catch Exceptions and improve msg, because tensorflow ist not able
-        # to preserve the traceback, i.e. the Exceptions does not contain any
-        # information where the Exception was raised.
-        nest.assert_shallow_structure(output_types, ret)
-        return nest.flatten(ret)
-
-    flat_values = tf.py_function(python_function_wrapper, flat_args, flat_output_types, name=name)
-
-    if output_shapes is not None:
-        # I am not sure if this is nessesary
-        output_shapes = nest.map_structure_up_to(output_types, tensor_shape.as_shape, output_shapes)
-
-    flattened_shapes = nest.flatten(output_shapes)
-    for ret_t, shape in zip(flat_values, flattened_shapes):
-        ret_t.set_shape(shape)
-
-    return nest.pack_sequence_as(output_types, flat_values)
-
-def from_indexable(iterator, output_types, output_shapes, num_parallel_calls=None, name=None):
-    ds = tf.data.Dataset.range(len(iterator))
-
-    def index_to_entry(index):
-        return py_func(
-                func=iterator.__getitem__,
-                args=(index,),
-                output_types=output_types,
-                output_shapes=output_shapes,
-                name=name)
-
-    return ds.map(index_to_entry, num_parallel_calls=num_parallel_calls)
 
 def run_eval(model, eval_dataset, eval_num_images, train_dir, global_step):
     @tf.function
@@ -252,7 +189,7 @@ def train():
             ds_eval = ds_impl.create_dataset_from_dir(FLAGS.train_data_dir, image_size, False)
 
             def create(ds):
-                return from_indexable(ds,
+                return map_iter.from_indexable(ds,
                         num_parallel_calls=FLAGS.num_cpus,
                         output_types=(tf.string, tf.float32, tf.uint8),
                         output_shapes=(
