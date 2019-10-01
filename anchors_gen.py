Fixed loss and metric calculation

anchors_gen.py
@@ -36,8 +36,8 @@ def generate_true_labels_for_anchors(orig_bboxes, orig_labels, anchors_all, outp
     scaled_size = image_size / yolo.DOWNSAMPLE_RATIO
 
     ious = loss.box_iou(orig_bboxes, anchors_all)
-    logger.info('ious: {}, orig_bboxes: {}, anchors_all: {}, output_xy_grids: {}, output_ratios: {}'.format(
-        ious.shape, orig_bboxes.shape, anchors_all.shape, output_xy_grids.shape, output_ratios.shape))
+    #logger.info('ious: {}, orig_bboxes: {}, anchors_all: {}, output_xy_grids: {}, output_ratios: {}'.format(
+    #    ious.shape, orig_bboxes.shape, anchors_all.shape, output_xy_grids.shape, output_ratios.shape))
 
     #tf.print('bboxes:', orig_bboxes)
 
@@ -53,7 +53,7 @@ def generate_true_labels_for_anchors(orig_bboxes, orig_labels, anchors_all, outp
     best_ratios = tf.gather(output_ratios, best_anchors_index)
     best_ratios = tf.expand_dims(best_ratios, 1) # [N, 1]
 
-    logger.info('best_anchors_index: {}, best_anchors: {}, best_xy_grids: {}, best_ratios: {}'.format(best_anchors_index.shape, best_anchors.shape, best_xy_grids.shape, best_ratios.shape))
+    #logger.info('best_anchors_index: {}, best_anchors: {}, best_xy_grids: {}, best_ratios: {}'.format(best_anchors_index.shape, best_anchors.shape, best_xy_grids.shape, best_ratios.shape))
 
     cx, cy, w, h = tf.split(orig_bboxes, num_or_size_splits=4, axis=1)
 
@@ -71,7 +71,7 @@ def generate_true_labels_for_anchors(orig_bboxes, orig_labels, anchors_all, outp
     y_for_loss = cy / best_ratios - grid_y
 
     #tf.print('x_for_loss:', x_for_loss)
-    logger.info('cx: {}, grid_x: {}, x_for_loss: {}, aw: {}, w: {}, w_for_loss: {}'.format(cx.shape, grid_x.shape, x_for_loss.shape, aw.shape, w.shape, w_for_loss.shape))
+    #logger.info('cx: {}, grid_x: {}, x_for_loss: {}, aw: {}, w: {}, w_for_loss: {}'.format(cx.shape, grid_x.shape, x_for_loss.shape, aw.shape, w.shape, w_for_loss.shape))
 
     bboxes_for_loss = tf.concat([x_for_loss, y_for_loss, w_for_loss, h_for_loss], axis=1)
     objs_for_loss = tf.ones_like(x_for_loss)

detection.py
@@ -319,7 +319,7 @@ def train():
 
             ds = ds.batch(FLAGS.batch_size)
             ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE).repeat()
-            #ds = dstrategy.experimental_distribute_dataset(ds)
+            ds = dstrategy.experimental_distribute_dataset(ds)
 
             logger.info('{} dataset has been created, tfrecords: {}'.format(name, len(filenames)))
 
@@ -338,7 +338,7 @@ def train():
             eval_dataset = create_dataset_from_tfrecord('eval', FLAGS.eval_tfrecord_dir, image_size, train_num_classes, is_training=False)
 
 
-        if True:
+        if False:
             draw_bboxes(image_size, train_dataset, train_cat_names, anchors_all, output_xy_grids, output_ratios)
             exit(0)
 
@@ -435,7 +435,7 @@ def train():
             y_true = true_values
             y_pred = logits
 
-            object_mask = tf.expand_dims(y_true[..., 4], 4)
+            object_mask = tf.expand_dims(y_true[..., 4], -1)
             object_mask_bool = tf.cast(object_mask[..., 0], 'bool')
             num_positive_labels_metric.update_state(tf.math.count_nonzero(object_mask))
 
@@ -465,17 +465,15 @@ def train():
 
             def get_best_ious(input_tuple, object_mask, true_bboxes):
                 idx, pred_boxes_for_single_image = input_tuple
-                # shape: [13, 13, 3, 4] & [13, 13, 3]  ==>  [V, 4]
-                # V: num of true gt box of each image in a batch
                 valid_true_boxes = tf.boolean_mask(true_bboxes[idx, ..., 0:4], tf.cast(object_mask[idx, ..., 0], 'bool'))
-                # shape: [13, 13, 3, 4] & [V, 4] ==> [13, 13, 3, V]
+                # shape: [N, 4] & [V, 4] ==> [N, V]
                 ious = loss.box_iou(pred_boxes_for_single_image, valid_true_boxes)
-                # shape: [13, 13, 3, V] -> [V]
-                best_ious = tf.reduce_max(ious, axis=[0, 1, 2])
+                # shape: [N, V] -> [V]
+                best_ious = tf.reduce_max(ious, axis=[0])
                 best_ious_padded = tf.pad(best_ious,
                         [[0, tf.cast(tf.math.count_nonzero(object_mask), tf.int32) - tf.shape(valid_true_boxes)[0]]],
                         constant_values=-1.)
-                mean_ious = tf.reduce_mean(ious, axis=[0, 1, 2])
+                mean_ious = tf.reduce_mean(ious, axis=[0])
                 mean_ious_padded = tf.pad(mean_ious,
                         [[0, tf.cast(tf.math.count_nonzero(object_mask), tf.int32) - tf.shape(valid_true_boxes)[0]]],
                         constant_values=-1.)

loss.py
@@ -48,14 +48,9 @@ def box_iou(pred_boxes, valid_true_boxes):
 
 def gen_ignore_mask(input_tuple, object_mask, true_bboxes):
     idx, pred_boxes_for_single_image = input_tuple
-    # shape: [13, 13, 3, 4] & [13, 13, 3]  ==>  [V, 4]
-    # V: num of true gt box of each image in a batch
     valid_true_boxes = tf.boolean_mask(true_bboxes[idx, ..., 0:4], tf.cast(object_mask[idx, ..., 0], 'bool'))
-    # shape: [13, 13, 3, 4] & [V, 4] ==> [13, 13, 3, V]
     iou = box_iou(pred_boxes_for_single_image, valid_true_boxes)
-    # shape: [13, 13, 3]
     best_iou = tf.reduce_max(iou, axis=-1)
-    # shape: [13, 13, 3]
     ignore_mask_tmp = tf.cast(best_iou < 0.5, tf.float32)
     #logger.info('pred_boxes_for_single_image: {}, valid_true_boxes: {}, iou: {}, best_iou: {}, ignore_mask_tmp: {}'.format(
     #    pred_boxes_for_single_image.shape, valid_true_boxes.shape, iou.shape, best_iou.shape, ignore_mask_tmp.shape))
@@ -67,9 +62,9 @@ class YOLOLoss:
                  grid_xy,
                  ratios,
                  image_size,
-                 obj_scale=5.,
-                 noobj_scale=1.,
-                 dist_scale=1.,
+                 obj_scale=2.,
+                 noobj_scale=1e-2,
+                 dist_scale=2.,
                  class_scale=1.,
                  **kwargs):
 
@@ -82,22 +77,14 @@ class YOLOLoss:
         self.dist_scale = dist_scale
         self.class_scale = class_scale
 
-        self.grid_xy = grid_xy
-        self.ratios = ratios
-        ax, ay, aw, ah = tf.split(anchors, 4, axis=1)
-        self.ax = ax
-        self.ay = ay
-        self.aw = aw
-        self.ah = ah
+        # added batch dimension
+        self.grid_xy = tf.expand_dims(grid_xy, 0)
+        self.ratios = tf.expand_dims(ratios, 0)
+        self.ratios = tf.expand_dims(ratios, -1) # [B, N, 1]
+        self.anchors_wh = tf.expand_dims(anchors[..., :2], 0)
 
-        self.anchors_wh = tf.concatenate([aw, ah], axis=1)
-
-        wh_scale = true_wh / self.image_size
-        # the smaller the box, the bigger the scale
-        self.wh_scale = tf.expand_dims(2 - wh_scale[..., 0] * wh_scale[..., 1], axis=-1)
-
-    def run(self, y_true, y_pred):
-        object_mask = tf.expand_dims(y_true[..., 4], 4)
+    def call(self, y_true, y_pred):
+        object_mask = tf.expand_dims(y_true[..., 4], -1)
         true_conf = object_mask
 
         #sigmoid(t_xy) + c_xy
@@ -128,8 +115,12 @@ class YOLOLoss:
         ignore_mask = tf.expand_dims(ignore_mask, -1)
 
 
-        dist_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true[..., :4], logits=y_pred[..., 4])
-        dist_loss = dist_loss * self.wh_scale * object_mask
+        wh_scale = true_wh / self.image_size
+        # the smaller the box, the bigger the scale
+        wh_scale = tf.expand_dims(2 - wh_scale[..., 0] * wh_scale[..., 1], axis=-1)
+
+        dist_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true[..., :4], logits=y_pred[..., :4])
+        dist_loss = dist_loss * wh_scale * object_mask
         dist_loss = tf.reduce_sum(dist_loss, [1, 2])
 
         smooth_true_classes = true_classes
@@ -148,6 +139,7 @@ class YOLOLoss:
         if num_nans != 0:
             tf.print('nans in class_loss3:', num_nans, 'class_loss:', tf.shape(class_loss), 'total nodes:', tf.math.reduce_prod(tf.shape(class_loss)))
 
+        logger.info('class_loss: {}'.format(class_loss.shape))
         class_loss = tf.reduce_sum(class_loss, [1, 2])
 
 
@@ -162,6 +154,6 @@ class YOLOLoss:
         conf_loss_pos = tf.reduce_sum(conf_loss_pos, [1, 2])
 
         conf_loss_neg = (1 - object_mask) * conf_loss * ignore_mask
-        conf_loss_neg = tf.reduce_sum(conf_loss_neg, list(range(1, 5)))
+        conf_loss_neg = tf.reduce_sum(conf_loss_neg, [1, 2])
 
         return dist_loss * self.dist_scale, class_loss * self.class_scale, conf_loss_pos * self.obj_scale, conf_loss_neg * self.noobj_scale

yolo.py
@@ -250,6 +250,7 @@ class Yolo3(tf.keras.Model):
     def __init__(self, params, num_classes, **kwargs):
         super(Yolo3, self).__init__(**kwargs)
 
+        self.num_classes = num_classes
         self.body = DarknetBody(params)
         self.head = DarknetHead(params, num_classes)
 
@@ -262,13 +263,15 @@ class Yolo3(tf.keras.Model):
         f5, f4, f3 = self.head(s3, s4, s5, training)
 
         batch_size = tf.shape(f5)[0]
-        last_dim = tf.shape(f5)[-1]
         outputs = []
         for output in [f5, f4, f3]:
-            flat = tf.reshape(output, [batch_size, -1, last_dim])
+            flat = tf.reshape(output, [batch_size, -1, 4+1+self.num_classes])
             outputs.append(flat)
 
+            logger.info('model output: {} -> {}'.format(output.shape, flat.shape))
+
         outputs = tf.concat(outputs, axis=1)
+        logger.info('model final output: {}'.format(outputs.shape))
         return outputs
 
 def local_relu(x):