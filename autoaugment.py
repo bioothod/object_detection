a9513e919a05cf94da52492bdd48118d43e21197
Evgeniy Polyakov <zbr@ioremap.net>
2019-12-06 18:06:24 +0000

    Fixed tf.nn.depthwise_conv2d() call

--- autoaugment.py
+++ autoaugment.py
@@ -326,8 +326,7 @@ def sharpness(image, factor):
     # Tile across channel dimension.
     kernel = tf.tile(kernel, [1, 1, 3, 1])
     strides = [1, 1, 1, 1]
-    degenerate = tf.nn.depthwise_conv2d(
-        image, kernel, strides, padding='VALID', rate=[1, 1])
+    degenerate = tf.nn.depthwise_conv2d(image, kernel, strides, padding='VALID', dilations=[1, 1])
     degenerate = tf.clip_by_value(degenerate, 0.0, 255.0)
     degenerate = tf.squeeze(tf.cast(degenerate, tf.uint8), [0])
 
