12b431b972c73bb03d97cc8679b747e7ec7a8869
Evgeniy Polyakov <zbr@ioremap.net>
2020-03-04 16:43:27 +0000

    Added saved_model and TRT freezing script

--- freeze.py
+++ freeze.py
@@ -4,6 +4,7 @@ import os
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
 import anchors_gen
 import encoder
@@ -22,10 +23,16 @@ logger.addHandler(__handler)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_classes', type=int, action='store', required=True, help='Number of classes')
+parser.add_argument('--image_size', type=int, default=528, help='Input image size')
 parser.add_argument('--model_name', type=str, action='store', required=True, help='Model name')
 parser.add_argument('--checkpoint', type=str, help='Load model weights from this file')
 parser.add_argument('--checkpoint_dir', type=str, help='Load model weights from the latest checkpoint in this directory')
-parser.add_argument('--output_dir', type=str, required=True, help='Path to directory, where images will be stored')
+parser.add_argument('--max_ret', type=int, default=100, help='Maximum number of returned boxes')
+parser.add_argument('--min_score', type=float, default=0.7, help='Minimal class probability')
+parser.add_argument('--min_obj_score', type=float, default=0.3, help='Minimal class probability')
+parser.add_argument('--min_size', type=float, default=4, help='Minimal size of the bounding box')
+parser.add_argument('--iou_threshold', type=float, default=0.45, help='Minimal IoU threshold for non-maximum suppression')
+parser.add_argument('--output_dir', type=str, help='Path to directory, where images will be stored')
 
 def normalize_image(image, dtype):
     image = tf.cast(image, dtype)
@@ -42,32 +49,65 @@ def main(argv=None):
         logger.error('You have to specify either checkpoint or checkpoint directory')
         exit(-1)
 
-    model = encoder.create_model(FLAGS.model_name, FLAGS.num_classes)
-    image_size = model.image_size
-    all_anchors, all_grid_xy, all_ratios = anchors_gen.generate_anchors(image_size, model.output_sizes)
+    class MyModel(tf.keras.Model):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
 
-    checkpoint = tf.train.Checkpoint(model=model)
+            self.model = encoder.create_model(FLAGS.model_name, FLAGS.num_classes)
 
-    status = checkpoint.restore(checkpoint_prefix)
-    status.assert_existing_objects_matched().expect_partial()
-    logger.info("Restored from external checkpoint {}".format(checkpoint_prefix))
+            self.all_anchors, self.all_grid_xy, self.all_ratios = anchors_gen.generate_anchors(FLAGS.image_size, self.model.output_sizes)
 
-    @tf.function(input_signature=[tf.TensorSpec([image_size * image_size * 3], tf.uint8)])
-    def control_flow(images):
-        images = tf.reshape(images, [-1, image_size, image_size, 3])
-        images = normalize_image(images, tf.float32)
+            checkpoint = tf.train.Checkpoint(model=self.model)
 
-        return eval_step_logits(model, images, image_size, FLAGS.num_classes, all_anchors, all_grid_xy, all_ratios)
+            status = checkpoint.restore(checkpoint_prefix)
+            status.assert_existing_objects_matched().expect_partial()
+            logger.info("Restored from external checkpoint {}".format(checkpoint_prefix))
 
-    dummy_image = tf.zeros((image_size * image_size * 3), dtype=tf.uint8)
-    control_flow(dummy_image)
+        @tf.function(input_signature=[tf.TensorSpec([None, FLAGS.image_size * FLAGS.image_size * 3], tf.uint8, name='model_input_images')])
+        def __call__(self, inputs):
+            images = tf.reshape(inputs, [-1, FLAGS.image_size, FLAGS.image_size, 3])
+            images = normalize_image(images, tf.float32)
 
-    to_export = tf.Module()
-    to_export.model = model
-    to_export.control_flow = control_flow
-    tf.saved_model.save(to_export, FLAGS.output_dir)
+            best_coords, best_scores, best_objs, best_cat_ids = eval_step_logits(self.model, images, FLAGS.image_size, FLAGS.num_classes,
+                    self.all_anchors, self.all_grid_xy, self.all_ratios,
+                    FLAGS.min_obj_score, FLAGS.min_score, FLAGS.min_size, FLAGS.max_ret, FLAGS.iou_threshold)
 
-    logger.info('Saved model into {}'.format(FLAGS.output_dir))
+            return best_coords, best_scores, best_objs, best_cat_ids
+
+
+    model = MyModel()
+
+    logger.info('model with {} backend has been created, image size: {}'.format(FLAGS.model_name, FLAGS.image_size))
+    #x = tf.keras.layers.Input(shape=(FLAGS.image_size * FLAGS.image_size * 3), dtype=tf.uint8, name='input/images')
+    x = tf.zeros((1, FLAGS.image_size * FLAGS.image_size * 3), dtype=tf.uint8)
+    _ = model(x)
+
+    output_dir = '{}_saved_model'.format(checkpoint_prefix)
+    tf.saved_model.save(model, output_dir)
+
+    logger.info('Saved model into {}'.format(output_dir))
+
+    conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS
+    conversion_params = conversion_params._replace(precision_mode="FP16")
+    conversion_params = conversion_params._replace(is_dynamic_op=True)
+
+    if False:
+        conversion_params = tf.experimental.tensorrt.ConversionParams(
+            rewriter_config_template=None,
+            max_workspace_size_bytes=tf.experimental.tensorrt.DEFAULT_TRT_MAX_WORKSPACE_SIZE_BYTES,
+            precision_mode=tf.experimental.tensorrt.TrtPrecisionMode.FP16,
+            minimum_segment_size=3,
+            is_dynamic_op=True,
+            maximum_cached_engines=1,
+            use_calibration=False,
+            max_batch_size=100,
+            allow_build_at_runtime=True
+        )
+
+
+    converter = trt.TrtGraphConverterV2(input_saved_model_dir=output_dir, conversion_params=conversion_params)
+    converter.convert()
+    converter.save('{}_trt'.format(output_dir))
 
 if __name__ == '__main__':
     FLAGS = parser.parse_args()
